5b3c16eb6bdc383cc5586c0a3ed9362e352e45ef
Jonas Fredriksson <jonas.fredriksson@combitech.com>
2025-11-03 13:29:29 +0100

    Working nvim nighlty with nix profile

--- misc/flake.nix
+++ misc/flake.nix
@@ -3,18 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay/800a400cde11cbdc9296bc3246869495560d2c9e";
   };
 
-  outputs = { nixpkgs, self, ... }: {
+  outputs = { nixpkgs, self, neovim-nightly-overlay, ... }: {
     packages.x86_64-linux.default = let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
         inherit system;
+        overlays = [ neovim-nightly-overlay.overlays.default ];
     };
     in pkgs.buildEnv {
       name = "my-dev-env";
       paths = with pkgs; [
         ripgrep
+        neovim
       ];
     };
   };
