17bb5c3ca3d7748f4868ba2ff27f3cf973d7631d
Jonas Fredriksson <jonas.fredriksson@combitech.com>
2025-11-19 07:56:37 +0100

    Working run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -9,12 +9,5 @@ mkdir -p "${HOST_CONFIG_DIR}"
 
 docker run -it --rm \
   --user 1000:1000 \
-  #-v "$HOST_CONFIG_DIR/home-manager:/home/ubuntu/.config/home-manager" \
-  #-v "$HOST_CONFIG_DIR/nix:/home/ubuntu/.config/nix" \
   nix_pde_test \
-  bash -c '
-    set -e
-    nix run home-manager/master -- switch --flake .config/home-manager#ubuntu
-
-    exec zsh
-  '
+  bash
